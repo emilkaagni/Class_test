35b93b2cd35c70d8c8ae56294bb00e8a01d72857
emilkaagni <basnyatemil02@gmail.com>
2024-05-06 12:31:54 +0545

    updated code

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,20 +1,3 @@
-// import 'package:class_test/screen/grid_view_screen.dart';
-// import 'package:class_test/screen/splash_screen.dart';
-// import 'package:flutter/material.dart';
-
-// class App extends StatelessWidget {
-//   const App({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return const MaterialApp(
-//       debugShowCheckedModeBanner: false,
-//       // themes
-//       home: SplashScreen(),
-//     );
-//   }
-// }
-
 import 'package:class_test/screen/splash_screen.dart';
 import 'package:flutter/material.dart';
 
